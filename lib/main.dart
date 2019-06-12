53c055514df7689fb515eb402a745d9ee2b2a580
Michal Czaplicki <mc222667@falmouth.ac.uk>
2019-06-12 20:52:24 +0100

    Testing, JSON serialization, and immutables

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget _buildItem(Article article) {
     return Padding(
+      key: Key(article.text), //uniq key identificator that helps identify witch widget is active
       padding: const EdgeInsets.all(16.0),
       child: ExpansionTile(
         title: new Text(
@@ -69,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 icon: Icon(Icons.launch),
                 color: Colors.green,
                 onPressed: () async {
-                  final fakeUrl = "http://${article.domain}";
+                  final fakeUrl = "http://${article.url}";
                   if (await canLaunch(fakeUrl)) {
                     await launch(fakeUrl);
                   }
--- lib/src/article.dart
+++ lib/src/article.dart
@@ -1,83 +1,91 @@
 class Article {
   final String text;
-  final String domain;
+  final String url;
   final String by;
-  final String age;
+  final String time;
   final int score;
   final int commentsCount;
 
-  const Article({
-    this.text,
-    this.domain,
-    this.by,
-    this.age,
-    this.score,
-    this.commentsCount
-  });
+  const Article(
+      {this.text, this.url, this.by, this.time, this.score, this.commentsCount});
 }
+
+//   factory Article.fromJson(Map<String, dynamic> json) {
+//     if (json == null) return null;
+
+//     return Article(
+//       text: json['text'] ?? '[null]',
+//       url: json['url'],
+//       by: json['by'],
+//       time: json['time'],
+//       score: json['score'] 
+//     );
+//   }
+// }
 
 final articles = [
   new Article(
     text: "Et proident nulla enim ut pariatur ullamco aute deserunt nisi.",
-    domain: "wp.pl",
+    url: "wp.pl",
     by: "ego",
-    age: "2 day",
+    time: "2 day",
     score: 81,
     commentsCount: 1324,
   ),
   new Article(
     text: "Ut qui voluptate et culpa deserunt aute veniam deserunt.",
-    domain: "onet.pl",
+    url: "onet.pl",
     by: "ego 1",
-    age: "2 day",
+    time: "2 day",
     score: 81,
     commentsCount: 57,
   ),
   new Article(
-    text: "Quis magna labore sit voluptate pariatur mollit in voluptate et Lorem.",
-    domain: "amazon.com",
+    text:
+        "Quis magna labore sit voluptate pariatur mollit in voluptate et Lorem.",
+    url: "amazon.com",
     by: "ego",
-    age: "2 day",
+    time: "2 day",
     score: 81,
     commentsCount: 2457,
   ),
   new Article(
     text: "Excepteur incididunt ex eu ipsum ex in.",
-    domain: "google.pl",
+    url: "google.pl",
     by: "ego",
-    age: "2 day",
+    time: "2 day",
     score: 81,
     commentsCount: 128,
   ),
   new Article(
     text: "Adipisicing duis nostrud occaecat cupidatat voluptate.",
-    domain: "wp.pl",
+    url: "wp.pl",
     by: "ego",
-    age: "2 day",
+    time: "2 day",
     score: 81,
     commentsCount: 48,
   ),
   new Article(
     text: "Magna cupidatat ea amet laboris.",
-    domain: "onet.pl",
+    url: "onet.pl",
     by: "ego",
-    age: "2 day",
+    time: "2 day",
     score: 81,
     commentsCount: 457,
   ),
   new Article(
     text: "Voluptate qui in do et sit et ea magna occaecat mollit.",
-    domain: "amazon.com",
+    url: "amazon.com",
     by: "ego",
-    age: "2 day",
+    time: "2 day",
     score: 81,
     commentsCount: 123,
   ),
   new Article(
     text: "Velit do consectetur do qui labore nostrud excepteur eiusmod esse.",
-    domain: "google.pl",
+    url: "google.pl",
     by: "ego",
-    age: "2 day",
+    time: "2 day",
     score: 81,
     commentsCount: 35,
   ),
