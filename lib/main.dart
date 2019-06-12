cfffc040054082afc7b4c25a2c9fc301a75a9fd5
Michal Czaplicki <mc222667@falmouth.ac.uk>
2019-06-12 18:27:36 +0100

    listView with dummy articles, setState, ListView and creating local variables

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'src/article.dart';
+
+import 'package:url_launcher/url_launcher.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Article> _articles = articles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(  // fake reloading
+        onRefresh: () async {
+          await new Future.delayed(
+            const Duration(seconds: 5), //wait 5 sec to pretent that it loading
+          );
+          setState(() {
+            _articles.removeAt(0);  //delating first iteam from the list
+          }); 
+        },
+        child: ListView(
+          children: _articles.map(_buildItem).toList(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItem(Article article) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ExpansionTile(
+        title: new Text(
+          article.text,
+          style: TextStyle(fontSize: 24),
+        ),
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              new Text("${article.commentsCount} comments"),
+              new IconButton(
+                icon: Icon(Icons.launch),
+                color: Colors.green,
+                onPressed: () async {
+                  final fakeUrl = "http://${article.domain}";
+                  if (await canLaunch(fakeUrl)) {
+                    await launch(fakeUrl);
+                  }
+                },
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/article.dart
@@ -0,0 +1,84 @@
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final int commentsCount;
+
+  const Article({
+    this.text,
+    this.domain,
+    this.by,
+    this.age,
+    this.score,
+    this.commentsCount
+  });
+}
+
+final articles = [
+  new Article(
+    text: "Et proident nulla enim ut pariatur ullamco aute deserunt nisi.",
+    domain: "wp.pl",
+    by: "ego",
+    age: "2 day",
+    score: 81,
+    commentsCount: 1324,
+  ),
+  new Article(
+    text: "Ut qui voluptate et culpa deserunt aute veniam deserunt.",
+    domain: "onet.pl",
+    by: "ego 1",
+    age: "2 day",
+    score: 81,
+    commentsCount: 57,
+  ),
+  new Article(
+    text: "Quis magna labore sit voluptate pariatur mollit in voluptate et Lorem.",
+    domain: "amazon.com",
+    by: "ego",
+    age: "2 day",
+    score: 81,
+    commentsCount: 2457,
+  ),
+  new Article(
+    text: "Excepteur incididunt ex eu ipsum ex in.",
+    domain: "google.pl",
+    by: "ego",
+    age: "2 day",
+    score: 81,
+    commentsCount: 128,
+  ),
+  new Article(
+    text: "Adipisicing duis nostrud occaecat cupidatat voluptate.",
+    domain: "wp.pl",
+    by: "ego",
+    age: "2 day",
+    score: 81,
+    commentsCount: 48,
+  ),
+  new Article(
+    text: "Magna cupidatat ea amet laboris.",
+    domain: "onet.pl",
+    by: "ego",
+    age: "2 day",
+    score: 81,
+    commentsCount: 457,
+  ),
+  new Article(
+    text: "Voluptate qui in do et sit et ea magna occaecat mollit.",
+    domain: "amazon.com",
+    by: "ego",
+    age: "2 day",
+    score: 81,
+    commentsCount: 123,
+  ),
+  new Article(
+    text: "Velit do consectetur do qui labore nostrud excepteur eiusmod esse.",
+    domain: "google.pl",
+    by: "ego",
+    age: "2 day",
+    score: 81,
+    commentsCount: 35,
+  ),
+];
